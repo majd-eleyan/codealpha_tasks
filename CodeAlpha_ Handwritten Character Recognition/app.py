732e823625c33ff70f808c8c1790b83bc78d688d
Majd Eleyan <majdtamer1@gmail.com>
2026-08-02 12:01:33 +0300

    Add streamlit app

--- CodeAlpha_ Handwritten Character Recognition/app.py
+++ CodeAlpha_ Handwritten Character Recognition/app.py
@@ -4,6 +4,7 @@ import streamlit as st
 from PIL import Image
 import tensorflow as tf
 from streamlit_drawable_canvas import st_canvas
+import os
 
 # Page configuration
 st.set_page_config(page_title="Digit Recognition", page_icon="✍️")
